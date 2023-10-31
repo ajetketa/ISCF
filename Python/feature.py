829a96e59235564b18137ca90176f478e64a3453
ajetketa <ajet.keta2000@gmail.com>
2023-10-31 17:20:17 +0100

    Added mean and variance as features to the dataset

--- Python/feature.py
+++ Python/feature.py
@@ -1,5 +1,39 @@
+import numpy as np
+import repository as r
 
 def mean(dataSample):
+    mean = np.empty((r.possibleMatrixLength[r.database], 17))
+    for i in range(np.shape(dataSample)[0]):
+        for j in range(np.shape(dataSample)[1]):
+            if j >= 1 and j <= np.shape(dataSample)[1] - 1:
+                data = dataSample[i, j]
+                mask = ~np.isnan(data)
+                if data[mask] is not None and np.shape(data[mask])[0] == 0:
+                    mean[i, j] = None
+                else:
+                    mean[i, j] = np.mean(data[mask])
+            else:
+                mean[i, j] = dataSample[i, j, 0, 0]
+
+    return mean
+
+def variance(dataSample):
+    variance = np.empty((r.possibleMatrixLength[r.database], 17))
+    for i in range(np.shape(dataSample)[0]):
+        for j in range(np.shape(dataSample)[1]):
+            if j >= 1 and j <= np.shape(dataSample)[1] - 1:
+                data = dataSample[i, j]
+                mask = ~np.isnan(data)
+                if data[mask] is not None and np.shape(data[mask])[0] == 0:
+                    variance[i, j] = None
+                else:
+                    variance[i, j] = np.var(data[mask])
+            else:
+                variance[i, j] = dataSample[i, j, 0, 0]
+
+    return variance
+
+
 
 
 
--- Python/main.py
+++ Python/main.py
@@ -1,8 +1,23 @@
 import repository as rep
+import feature as f
+import numpy as np
+from tabulate import tabulate
+import matplotlib.pyplot as plt
+
+headers = ["HP", "IRDE", "IRFE", "IRBA", "BDE", "BFE", "BBA", "OR3DE", "OR3FE", "OR3BA", "OR6DE", "OR6FE", "OR6BA", "OR12DE", "OR12FE", "OR12BA", "Diameter"]
 
 def import_data():
     sampleMatrix = rep.import_data()
-    print(sampleMatrix)
+    mean = f.variance(sampleMatrix)
+    print("Mean")
+    print(np.shape(mean))
+    print(mean)
+    table = tabulate(mean, headers)
+    print(table)
+
+    plt.plot(mean[:, 13], marker='o')
+    plt.show()
+
 
 if __name__ == '__main__':
     import_data()
